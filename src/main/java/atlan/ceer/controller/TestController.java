3221939fc35f828c7f5237f7c016af5e9c1c21ba
atlan <atlanceer@outlook.com>
2019-09-29 23:02:14 +0800

    添加错误处理、配置日志等

--- src/main/java/atlan/ceer/controller/TestController.java
+++ src/main/java/atlan/ceer/controller/TestController.java
@@ -1,12 +1,31 @@
 package atlan.ceer.controller;
 
+import atlan.ceer.exception.NotFoundException;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class TestController {
-    @RequestMapping("/test")
-    public void test(){
+    @RequestMapping("/testError")
+    public void testError(){
         int i=9/0;
     }
+
+    @RequestMapping("/testNotFound")
+    public void testNotFound(){
+        //int i=9/0;
+        String blog=null;
+        if (blog==null) {
+            throw new NotFoundException("没有找到页面");
+        }
+    }
+
+    @RequestMapping("/{id}/{name}")
+    public String testAop(@PathVariable Integer id, @PathVariable String name){
+        System.out.println("--------index--------"+id+name);
+        return "index";
+    }
+
 }
--- src/main/java/atlan/ceer/handler/ControllerExceptionHandler.java
+++ src/main/java/atlan/ceer/handler/ControllerExceptionHandler.java
@@ -1,19 +1,29 @@
 package atlan.ceer.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * 处理错误页面
+ */
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ModelAndView exceptionHandler(HttpServletRequest request,Exception e){
+    public ModelAndView exceptionHandler(HttpServletRequest request,Exception e) throws Exception {
         log.error("Request URL : {}, Exception : {}",request.getRequestURL(), e);
+
+        //注解工具判断是否还有ResponseStatus注解
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null){
+            throw e;
+        }
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("url", request.getRequestURL());
